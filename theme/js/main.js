0ee425e6d6ecd7abd3700151c2e4e559529ecfc7
Luke Gessler <lukegessler@gmail.com>
2025-03-09 13:49:49 -0400

    Update website content Sun 09 Mar 2025 01:49:49 PM EDT

---
+++ theme/js/main.js
@@ -0,0 +1,5 @@
+// Add any JavaScript functionality here
+document.addEventListener('DOMContentLoaded', function() {
+    // Code to execute when the DOM is fully loaded
+    console.log('Academic theme loaded');
+}); 
