5d6a4bbe77c034c9dadd9543808161c672097d2e
Luke Gessler <lukegessler@gmail.com>
2025-03-09 13:54:20 -0400

    don"t keep output folder

--- output/theme/js/main.js
+++
@@ -1,5 +0,0 @@
-// Add any JavaScript functionality here
-document.addEventListener('DOMContentLoaded', function() {
-    // Code to execute when the DOM is fully loaded
-    console.log('Academic theme loaded');
-}); 
